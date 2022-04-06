081964fccef18bb6e54b504587b9a48778597878
Steellkek <alya.min80@gmail.com>
2022-04-06 21:05:19 +0300

    Initial commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            var a = new DummyRequestHandler();
+            Console.WriteLine("Программа начала работу");
+            Console.WriteLine("Введите текст запроса. Для остановки программы напишите /exit");
+            //сообщение
+            string command = Console.ReadLine();
+
+            while (command != "/exit")
+            {
+                //ввод аргументов
+                var arguments = new string[100];
+                Console.WriteLine("Введите аргументы запроса. Для остановки добавления аргументов напишите /end");
+                var argument = Console.ReadLine();
+                var i = 0;
+                while (argument!="/end")
+                {
+                    arguments[0] = argument;
+                    Console.WriteLine("Введите следующий аргумент запроса. Для остановки добавления аргументов напишите /end");
+                    argument = Console.ReadLine();
+                    i += 1;
+                }
+                //запуск потока
+                new Thread(() => Go(a, command, arguments)).Start();
+                Console.WriteLine("Введите текст запроса. Для остановки программы напишите /exit");
+                command = Console.ReadLine();
+
+            }
+            Console.WriteLine("Программа завершила работу.");
+
+        }
+
+        public static void Go(DummyRequestHandler a, string command, string[] arguments)
+        {
+            var id = Guid.NewGuid().ToString("D");
+            Console.WriteLine($"Было отправлено сообщение '{command}'. Присвоен индефикатор {id}");
+            try
+            {
+                var id1 = a.HandleRequest(command, arguments);
+                Console.WriteLine($"Сообщение с индефикатором {id} получило ответ {id1}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Сообщение с индефикатором {id} упало с ошибкой: {e.Message} ");
+            }
+        }
+        
+    }
+
+}
